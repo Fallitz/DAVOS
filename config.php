20ba801373ad965ffcc5282d71922ccb7e3ef9a5
Fallitz <47279161+Fallitz@users.noreply.github.com>
2022-02-08 23:44:04 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,18 +1,13 @@
 <?php
 
 //Dados para conexão com banco de dados mysql/mariaDB
-$db="atacadao";
-$host= "";
-$user= "";
-$password= "";
-
-$nomeDaFilial="Aracaju Tancredo Neves";         //Nome que será exibido nos relatórios
-$adminLogin="1";                                //Login que será usado para fazer login na área administrativa do portal papafila
-$adminPassword="1";                             //Senha que será usada para fazer login na área administrativa do portal papafila
+$db="davos";
+$host= "localhost";
+$user= "root";
+$password= "davos123"; //ÓTIMA SENHA xD
 
 //Definir nomes das tabelas no banco de dados - CUIDADO AO MUDAR, é recomendado manter o padrão
 $usuariosDB = "usuarios";
 $historicoDB = "historico";
-$estoqueDB = "estoque";
 
 ?>
