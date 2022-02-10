396c7b9fb470f9044eb72b86f0586efe63601e61
Fallitz <47279161+Fallitz@users.noreply.github.com>
2022-02-10 00:57:35 -0300

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 $db="davos";
 $host= "localhost:3306";
 $user= "root";
-$password= "324521603"; //b1wlNmjqn8he
+$password= "b1wlNmjqn8he"; //b1wlNmjqn8he
 
 //Definir nomes das tabelas no banco de dados - CUIDADO AO MUDAR, é recomendado manter o padrão
 $usuariosDB = "alunos";
