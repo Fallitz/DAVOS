d2fdb6a83a152ed6bcc64a2dca25cef01ed5bcd8
Fallitz <47279161+Fallitz@users.noreply.github.com>
2022-02-09 23:47:53 -0300

    Update index.php

--- page/admin/index.php
+++ page/admin/index.php
@@ -9,9 +9,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap-441-dist-css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
-    <link rel="icon" href="../../assets/images/icon.png">
+    <link rel="stylesheet" type="text/css" href="./../..assets/css/bootstrap-441-dist-css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./../..assets/css/style.css">
+    <link rel="icon" href="./../..assets/images/icon.png">
    
     <title>DAVOS LEARN - Register</title>
 </head>
@@ -21,7 +21,7 @@
             <div class="container-interface">                
                 <div class="content" id="content">
                     <div class="header">
-                        <img class="logo" id="logo" src="../../assets/images/logo2.png"></img>     
+                        <img class="logo" id="logo" src="./../..assets/images/logo2.png"></img>     
                         <div class="header-right">
                             <a href="/" target="_self" id="home" onclick="openHome()">Home</a>
                             <a id="register" href="/register" target="_self" onclick="openCadastro()">Register</a>
@@ -40,10 +40,10 @@
                 </div>
                 <div class="msgErrors">
                     <div class="msgBox" id="msgBox">
-                        <img class="imgError" id="imgMsgBox" src="../../assets/images/atencao.png" alt="">
+                        <img class="imgError" id="imgMsgBox" src="./../..assets/images/atencao.png" alt="">
                         <div class="msg" id="msg"></div>
                         <div class="closeMsg">
-                            <img class="imgClose" id="imgClose" src="../../assets/images/closeMsg.png" onclick="closeError()" alt="">
+                            <img class="imgClose" id="imgClose" src="./../..assets/images/closeMsg.png" onclick="closeError()" alt="">
                         </div>
                     </div>
                 </div>
